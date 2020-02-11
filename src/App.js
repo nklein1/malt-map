dc1caaed7cd7ec599249cfbcb3e11a46b5b7fd2f
Nick Klein <nklein@aceticket.com>
2020-02-11 16:51:58 -0500

    Enabled app as Github page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { HashRouter, Route } from "react-router-dom";
 
-import HomePage from './containers/HomePage/HomePage.jsx';
+import HomePage from './containers/homepage/homepage.jsx';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <HashRouter basename='/'>
+      <HashRouter basename='/malt-map'>
         <Route exact path='/' component={HomePage} />
       </HashRouter>
     </div>
