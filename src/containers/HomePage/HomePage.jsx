25646d872b133df55f0bfe53f7418b10cdc9fcae
Nick Klein <nklein@aceticket.com>
2020-02-11 22:11:44 -0500

    Fleshed out initial chart with legend and styling fixes

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import classNames from 'classnames';
 // import { Link } from 'react-router-dom';
 import {
   VictoryChart,
   VictoryScatter,
   VictoryAxis,
+  VictoryContainer,
+  VictoryClipContainer,
+  VictoryZoomContainer,
   // VictoryTooltip,
   VictoryLabel
 } from 'victory';
 
 import { sampleData } from '../../data/sampleData.js';
-import styles from './HomePage.module.scss';
+import styles from './homepage.module.scss';
 
 class HomePage extends React.Component {
 
@@ -48,22 +52,29 @@ class HomePage extends React.Component {
   renderChart = (data) => {
     if (this.state.sampleData) {
       return (
-        <VictoryChart>
+        <VictoryChart
+            padding={{top: 10, bottom: 10, left: 25, right: 25}}
+            containerComponent={
+            <VictoryZoomContainer
+                minimumZoom={{x: 40, y: 40}}
+                clipContainerComponent={
+                  <VictoryClipContainer clipPadding={{top: 25, right: 25, left: 25, bottom: 25}} />
+                }
+            />
+        }>
           <VictoryAxis
               crossAxis
-              domain={{ x: [80, -80], y: [80, -80] }}
               style={{ tickLabels: {display: 'none'} }}
           />
             <VictoryScatter
-                style={{ data: { fill: ({ datum }) => this.getFillColor(datum) } }}
-                size={2}
+                data={this.state.sampleData}
                 x={'peatScore'}
                 y={'colorScore'}
                 name={'whiskyMap'}
-                domain={{ x: [80, -80], y: [80, -80] }}
-                // labelComponent={
-                //   <VictoryTooltip />
-                // }
+                domain={{ x: [90, -90], y: [90, -90] }}
+                size={2}
+                style={{ data: { fill: ({ datum }) => this.getFillColor(datum) } }}
+                labels={({ datum }) => datum.name}
                 labelComponent={
                   <VictoryLabel
                       dy={-3}
@@ -73,34 +84,53 @@ class HomePage extends React.Component {
                       text={({ datum }) => `${datum.name}`}
                   />
                 }
-                data={this.state.sampleData}
-                labels={({ datum }) => datum.name}
+                // labelComponent={
+                //   <VictoryTooltip />
+                // }
             />
           <VictoryAxis
               crossAxis
               dependentAxis
-              domain={{ x: [80, -80], y: [80, -80] }}
-              style={{ tickLabels: {display: "none"} }}
+              style={{ tickLabels: {display: 'none'} }}
           />
         </VictoryChart>
-
       );
     }
   }
 
   render() {
     return (
-      <div className={styles.parentContainer}>
-        <div className={styles.outerContainer}>
-          <div className={styles.title}>Darker Color</div>
-          <div className={styles.innerContainer}>
-            <div className={styles.title}>More Peat Smoke</div>
-            <div className={styles.chartContainer}>
-              {this.renderChart()}
-            </div>
-            <div className={styles.title}>Less Peat Smoke</div>
+      <div className={styles.rootContainer}>
+        <div className={styles.infoContainer}>
+          <div className={styles.pageTitle}>The Scotch Palate Map</div>
+          <div className={styles.pageSubtitle}>(Scroll to Zoom)</div>
+          <div className={styles.legendTitle}>Region:</div>
+          <div className={styles.legendTitle}>
+            <div className={classNames(styles.legendItem, styles.speyside)}>Speyside</div>
+            <div className={classNames(styles.legendItem, styles.highland)}>Highland</div>
+            <div className={classNames(styles.legendItem, styles.islay)}>Islay</div>
+            <div className={classNames(styles.legendItem, styles.island)}>Island</div>
+            <div className={classNames(styles.legendItem, styles.campbeltown)}>Campbeltown</div>
+            <div className={classNames(styles.legendItem, styles.lowland)}>Lowland</div>
+            <div className={classNames(styles.legendItem, styles.other)}>Other</div>
           </div>
-          <div className={styles.title}>Lighter Color</div>
+        </div>
+        <div className={styles.parentContainer}>
+          <div className={styles.outerContainer}>
+            <div className={styles.title}>Darker & Richer</div>
+            <div className={styles.innerContainer}>
+              <div className={styles.title}>
+                <div className={styles.titleChild}>More Peat Smoke</div>
+              </div>
+              <div className={styles.chartContainer}>
+                {this.renderChart()}
+              </div>
+              <div className={styles.title}>
+                <div className={styles.titleChild}>Less Peat Smoke</div>
+              </div>
+            </div>
+            <div className={styles.title}>Lighter & Drier</div>
+          </div>
         </div>
       </div>
     );
